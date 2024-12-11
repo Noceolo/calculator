1469f030b56ae760dd14546d7ef9da9d23bf8352
Noceolo <eliottlm@hotmail.fr>
2024-12-11 17:19:16 +0100

    Created the script file

---
+++ script.js
@@ -0,0 +1,32 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const substract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+let number1 
+let operator
+let number2
+
+const operate = function(operator, number1, number2){
+    if (operator === '+'){
+        add(number1, number2);
+    }else if (operator === '-'){
+        substract(number1, number2)
+    }else if (operator === '*'){
+        multiply(number1, number2);
+    }else if (operator === '/'){
+        divide(number1, number2);
+    }
+
+}
