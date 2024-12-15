ab81df09dbe4471ee67bfbe553c94096b1a46f2a
Noceolo <eliottlm@hotmail.fr>
2024-12-15 14:26:49 +0100

    Added event listener fct to buttons, cleaned fct

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-const add = function(a, b) {
-    return a + b;
-}
+const add = (a, b) => a + b;
 
-const substract = function(a, b) {
-    return a - b;
-}
+const substract = (a, b) => a - b;
 
-const multiply = function(a, b) {
-    return a * b;
-}
+const multiply = (a, b) => a * b;
 
-const divide = function(a, b) {
-    return a / b;
-}
+const divide = (a, b) => a / b;
 
-let number1 
-let operator
-let number2
-let display
 
-function eraseText() {
-    document.getElementById("screen").textContent = "";
-    number1 = 0
-    operator = 0
-    number2 = 0
-}
+let number1 = null;
+let operator = null;
+let number2 = null;
 
 
 
+const screen = document.getElementById("screen");
+const numberButtons = document.querySelectorAll(".num");
+const operatorButtons = document.querySelectorAll(".operator")
 
+numberButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const number = event.target.getAttribute("data-number");
+        if (screen.textContent === "...") {
+            screen.textContent = number;
+        }else {
+        screen.textContent += number;
+        }
+    });
+});
 
 const operate = function(operator, number1, number2){
     if (operator === '+'){
-        add(number1, number2);
+        return add(number1, number2);
     }else if (operator === '-'){
-        substract(number1, number2)
+        return substract(number1, number2)
     }else if (operator === '*'){
-        multiply(number1, number2);
+        returnmultiply(number1, number2);
     }else if (operator === '/'){
-        divide(number1, number2);
-    }
-
+        if ((number1 = 0) || (number1 = null) || (number2 = 0) || (number2 = null)){
+            screen.textContent = "ERROR"
+        }
+        return divide(number1, number2);
+    };
 }
 
-// clearButton.addEventListener("click", eraseText());
+
+
+
+function eraseText() {
+    document.getElementById("screen").textContent = "...";
+    number1 = null
+    operator = null
+    number2 = null
+}
+
+
+
+
+
+
+
+
 
