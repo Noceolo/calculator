9e9e8894ab47db8a6b1e9798a5998b9018896a5b
Noceolo <eliottlm@hotmail.fr>
2025-01-08 17:50:28 +0100

    Added variables to hold numbers, and operators fct

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 
-let number1 = null;
-let operator = null;
-let number2 = null;
+var number1 = null;
+var operator = null;
+var number2 = null;
 
 
 
@@ -20,7 +20,7 @@ const operatorButtons = document.querySelectorAll(".operator")
 numberButtons.forEach(button => {
     button.addEventListener("click", event => {
         const number = event.target.getAttribute("data-number");
-        if (screen.textContent === "...") {
+        if (screen.textContent === "..." || screen.textContent === "-" || screen.textContent === "+" || screen.textContent === "*" || screen.textContent === "/") {
             screen.textContent = number;
         }else {
         screen.textContent += number;
@@ -28,18 +28,52 @@ numberButtons.forEach(button => {
     });
 });
 
-const operate = function(operator, number1, number2){
+// operatorButtons.forEach(button => {
+//     button.addEventListener("click", event => {
+//         if (screen.textContent != "...") {
+//             number1 = screen.textContent;
+//             if (operator != null) {
+//             number2 = screen.textContent;
+//             }else {
+//             operator = event.target.getAttribute("data-operator")
+//             }
+//         }
+//         screen.textContent = " ";
+        
+
+//     });
+// });
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        if (operator === null){
+            operator = event.target.getAttribute("data-operator")
+            number1 = screen.textContent;
+            screen.textContent = " ";
+        }else {
+            number2 = screen.textContent
+        }
+    });
+});    
+
+const operate = function(){
+    number2 = screen.textContent
     if (operator === '+'){
-        return add(number1, number2);
+        screen.textContent = +number1 + +number2;
+
     }else if (operator === '-'){
-        return substract(number1, number2)
+        screen.textContent = +number1 - +number2;
+
     }else if (operator === '*'){
-        returnmultiply(number1, number2);
+        screen.textContent = +number1 * +number2;
+
     }else if (operator === '/'){
         if ((number1 = 0) || (number1 = null) || (number2 = 0) || (number2 = null)){
             screen.textContent = "ERROR"
         }
-        return divide(number1, number2);
+        result = +number1 / +number2;
+        parseFloat(result);
+        screen.textContent = result;
     };
 }
 
@@ -51,6 +85,7 @@ function eraseText() {
     number1 = null
     operator = null
     number2 = null
+    result = null
 }
 
 
