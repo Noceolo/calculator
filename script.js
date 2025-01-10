4bf8fe7cdb9ddbfe2a589174f66f0297106ecbed
Noceolo <176486244+Noceolo@users.noreply.github.com.>
2025-01-10 16:01:26 +0100

    Changed the operate function to script function Switched function place, trying to fix the divide giving NaN

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ const substract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => (b === 0 ? "error" : a / b);
 
 
-var number1 = null;
-var operator = null;
-var number2 = null;
+let number1 = null;
+let operator = null;
+let number2 = null;
 
+const operate = function(operator, number1, number2){
+    number2 = parseFloat(number2);
+    number1 = parseFloat(number1);
+    if (operator === '+'){
+        return add(number1,number2)
 
+    }else if (operator === '-'){
+        return substract(number1,number2)
+
+    }else if (operator === '*'){
+        return multiply(number1,number2)
+
+    }else if (operator === '/'){
+        if ((number1 = 0) || (number1 = null) || (number2 = 0) || (number2 = null)){
+            screen.textContent = "ERROR"
+        }
+        return divide(number1,number2)
+    };
+}
 
 const screen = document.getElementById("screen");
 const numberButtons = document.querySelectorAll(".num");
 const operatorButtons = document.querySelectorAll(".operator")
+const resultButton = document.getElementById("result")
 
 numberButtons.forEach(button => {
     button.addEventListener("click", event => {
@@ -36,29 +55,23 @@ operatorButtons.forEach(button => {
             number1 = screen.textContent;
             screen.textContent = " ";
         }else {
-            number2 = screen.textContent
+            number2 = parseFloat(screen.textContent);
         }
     });
 });    
 
-const operate = function(){
-    number2 = screen.textContent
-    if (operator === '+'){
-        screen.textContent = add(number1,number2)
 
-    }else if (operator === '-'){
-        screen.textContent = substract(number1,number2)
 
-    }else if (operator === '*'){
-        screen.textContent = multiply(number1,number2)
-
-    }else if (operator === '/'){
-        if ((number1 = 0) || (number1 = null) || (number2 = 0) || (number2 = null)){
-            screen.textContent = "ERROR"
-        }
-        screen.textContent = divide(number1,number2)
+resultButton.addEventListener("click", () => {
+    if(number1 && operator) {
+        number2 = screen.textContent;
+        const result = operate(operator, number1, number2);
+        screen.textContent = result;
+        number1 = null;
+        number2 = null;
+        operator = null;
     };
-}
+})
 
 
 
